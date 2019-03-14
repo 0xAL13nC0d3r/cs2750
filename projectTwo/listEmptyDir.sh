c741b325db1e391a29f79d04a800df3688678200
Matt Cassell <mpc5h3@umsl.edu>
2019-03-13 21:17:47 -0500

    partOne completed, partThree started, also some sample files were added

--- projectTwo/listEmptyDir.sh
+++ projectTwo/listEmptyDir.sh
@@ -2,27 +2,21 @@
 #Matt Cassell
 
 clear
-if [ -z $1 ];then
+if [ -z $1 ] || [ ! -d $1 ];then
 	echo "usage <directory name>"
 	exit
 fi
 
-#DO WE NEED TO CHECK TO SEE IF THIS FILE EXISTS? TAKES A WHILE...
-#WOULD BE MUCH EASIER TO ASSUME THEY ARE IN PASSED IN DIRECTORY
-#DIRECTORY=$(find / -type d -name $1 2>/dev/null)
-#echo $DIRECTORY
-
-CURRENT_PATH=$(pwd)
-FILES=$CURRENT_PATH/*
+echo $1
+FILES=$1/*
+echo $FILES
 
 for f in $FILES
 do
-	if [ -d $f ];then
-		if [ ! $(ls -A $f) ];then
-			echo $f  | sed 's:.*/::' >> EmptyDir.txt
-		fi
-	fi	
-
+	if [ ! $(ls -A $f) ];then
+		echo $f  | sed 's:.*/::' >> EmptyDir.txt
+	fi
+	
 	echo $f | sed 's:.*/::'
 done
  
